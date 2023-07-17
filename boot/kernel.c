ea36861e2c30d07d5646a7306636727fca374a6c
Lucas <lrassbachson@gmail.com>
2023-07-16 18:07:15 -0700

    added newline support for the terminal

--- boot/kernel.c
+++ boot/kernel.c
@@ -85,12 +85,18 @@ void terminal_putentryat(char c, uint8_t color, size_t x, size_t y)
 
 void terminal_putchar(char c)
 {
+	if (c == '\n'){
+		terminal_column = 0;
+		terminal_row++;
+		return;
+	}
 	terminal_putentryat(c, terminal_color, terminal_column, terminal_row);
 	if (++terminal_column == VGA_WIDTH) {
 		terminal_column = 0;
 		if (++terminal_row == VGA_HEIGHT)
 			terminal_row = 0;
 	}
+	
 }
 
 void terminal_write(const char* data, size_t size)
@@ -110,7 +116,7 @@ void kernel_main(void)
 	terminal_initialize();
 
 	// no newline support as of now
-	terminal_writestring("Hello kernel!");
+	terminal_writestring("Hello kernel! \n added a new line. hopefully it works");
 }
 
 
