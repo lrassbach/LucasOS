ce050e54e38926b7983efcf0edbf49007207232c
Lucas <lrassbachson@gmail.com>
2023-07-16 17:56:50 -0700

    Fixed a typo in the terminal initialization in the kernel

--- boot/kernel.c
+++ boot/kernel.c
@@ -62,8 +62,8 @@ void terminal_initialize(void)
 {
 	terminal_row = 0;
 	terminal_column = 0;
-	terminal_color = vga_entry_color(VGA_COLOR_WHITE, VGA_COLOR_BLACK);
-	terminal_buffer = (uint16_t*) 0XB800;
+	terminal_color = vga_entry_color(VGA_COLOR_LIGHT_GREY, VGA_COLOR_BLACK);
+	terminal_buffer = (uint16_t*) 0XB8000;
 	for (size_t y = 0; y < VGA_HEIGHT; y++) {
 		for (size_t x = 0; x < VGA_WIDTH; x++) {
 			const size_t index = y * VGA_WIDTH + x;
@@ -110,7 +110,7 @@ void kernel_main(void)
 	terminal_initialize();
 
 	// no newline support as of now
-	terminal_writestring("Hello kernel!\n");
+	terminal_writestring("Hello kernel!");
 }
 
 
