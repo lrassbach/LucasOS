ea5f1d578ec7dd860b0a1ba769674703884b7f0e
Lucas <lrassbachson@gmail.com>
2023-07-30 13:22:44 -0700

    typo fix

--- libc/stdio/putchar.c
+++ libc/stdio/putchar.c
@@ -7,7 +7,7 @@
 int putchar(int ic) {
 #if defined(__is_libk)
     char c = (char) ic;
-    terminal_write(&c, sizeof(c))
+    terminal_write(&c, sizeof(c));
 #else
     // TODO implement stdio and the write system call
 #endif
